0cb9be8703fdc594e2c5e4a7502bfd29ae11a267
laibinglin <1154246882@qq.com>
2026-02-28 10:40:41 +0800

    feat(ioc): implement dependency injection for Engine classes and update configuration
    - Added @injectable() decorators to GasEngine and ElectricEngine classes for dependency injection.
    - Updated main.ts to bind ElectricEngine to the "Engine" identifier and adjusted Car binding.
    - Modified tsconfig.json to change module settings for better compatibility with ES modules.
    - Enhanced package.json with a new script for running TypeScript files using tsx.

--- ioc/Engine.ts
+++ ioc/Engine.ts
@@ -1,13 +1,18 @@
+import { injectable } from "inversify";
+
+
 export interface Engine {
   run(): void;
 }
 
+@injectable()
 export class GasEngine implements Engine {
   run(): void {
     console.log("GasEngine is running");
   }
 }
 
+@injectable()
 export class ElectricEngine implements Engine {
   run(): void {
     console.log("ElectricEngine is running");
--- ioc/main.ts
+++ ioc/main.ts
@@ -1,6 +1,16 @@
+import "reflect-metadata";
 import { Container } from "inversify";
 import { Car } from "./Car";
+import { ElectricEngine, GasEngine } from "./Engine";
 
 const container = new Container();
 
-container.bind(Car).toSelf();
+// 绑定 Engine 服务到 GasEngine 实现
+container.bind("Engine").to(ElectricEngine);
+
+// 绑定 Car
+// container.bind(Car).toSelf();
+container.bind('Car').to(Car);
+
+const car = container.get<Car>("Car");
+car.start();
